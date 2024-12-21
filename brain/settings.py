805555b98cf3d2f4813dcc6880a3fb6986443705
Debadrita-rgb <debadritapaul76@gmail.com>
2024-12-21 19:05:24 +0530

    first_commit

--- brain/settings.py
+++ brain/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path,os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,15 +84,18 @@ WSGI_APPLICATION = 'brain.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ten_brain_research',
+#         'HOST': 'localhost',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'PORT': '3306',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ten_brain_research',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'PORT': '3306',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 # DATABASES = {
 #     'default': {
